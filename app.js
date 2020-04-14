6a3bf4558ed45eb453ad6531a84e38af663adc9b
vormc2005 <voronov.dmitry@gmail.com>
2020-04-14 14:42:15 -0400

    Pgi game finished

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ scores[activePlayer] += roundScore;
 document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
 //Check if the palyaer won the game
-if (scores[activePlayer] >= 10) {
+if (scores[activePlayer] >= 100) {
     document.querySelector('#name-' + activePlayer).textContent = 'Winner';
     document.querySelector('.dice').style.display = 'none';
     document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
